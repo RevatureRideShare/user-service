23f42b5fecd8415e41b377a0444f4692ce6623fe
Shalom Nguyen <nguys2276@gmail.com>
2019-11-16 18:47:50 -0500

    test(UserServiceApplicationTests, testteststage): reverted USAT to original state. added trivial test

--- src/test/java/com/example/demo/UserServiceApplicationTests.java
+++ src/test/java/com/example/demo/UserServiceApplicationTests.java
@@ -1,21 +1,13 @@
 package com.example.demo;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UserServiceApplicationTests {
-
-	 @Test
-	 void contextLoads() {
-	 }
-
+	
 	@Test
-	public void test() {
-		//this is a demo test and should always pass
-		assertTrue(true);
-	}	
+	void contextLoads() {
+	}
 
 }
---
+++ src/test/java/com/example/test/testteststage.java
@@ -0,0 +1,36 @@
+package com.example.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class testteststage {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		assertTrue(true);
+	}
+
+}
