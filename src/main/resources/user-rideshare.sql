540de6c811a93ab71757afb0977d2727c6c16007
unknown <mcbride.mik@gmail.com>
2019-11-25 18:55:10 -0500

    chore(user-rideshare.sql) Adding .sql file for making the table.

---
+++ src/main/resources/user-rideshare.sql
@@ -0,0 +1,25 @@
+create table users(
+user_id numeric primary key,
+email varchar(50) unique not null,
+first_name varchar(50) not null,
+last_name varchar(50) not null,
+phone_number varchar(20) not null,
+role varchar(25) not null,
+ride_status varchar(25) not null,
+account_status boolean,
+location_id numeric not null,
+car_id numeric references car(car_id)
+);
+
+create table car(
+car_id numeric primary key,
+seats int not null
+);
+
+create sequence user_id_seq;
+drop sequence user_id_seq;
+create sequence car_id_seq;
+drop sequence car_id_seq;
+
+drop table users;
+drop table car;
