afc6b809948c5c3bd62eb7d96bec7c0389f090f4
unknown <mcbride.mik@gmail.com>
2019-11-26 19:01:07 -0500

    feature(.sql) Fixing the .sql file to match with the enums and original design.

--- src/main/resources/user-rideshare.sql
+++ src/main/resources/user-rideshare.sql
@@ -1,19 +1,19 @@
-create table car(
-car_id numeric primary key,
-seats int not null
-);
-
 create table users(
 user_id numeric primary key,
 email varchar(50) unique not null,
 first_name varchar(50) not null,
 last_name varchar(50) not null,
 phone_number varchar(20) not null,
-role varchar(25) not null,
-ride_status varchar(25) not null,
+role numeric not null,
+ride_status numeric not null,
 account_status boolean,
-location_id numeric not null,
-car_id numeric references car(car_id)
+location_id numeric not null
+);
+
+create table car(
+car_id numeric primary key,
+user_id numeric references users(user_id),
+seats numeric not null
 );
 
 create sequence user_id_seq;
@@ -22,5 +22,5 @@ create sequence car_id_seq;
 --drop sequence user_id_seq;
 --drop sequence car_id_seq;
 
---drop table users;
 --drop table car;
+--drop table users;
