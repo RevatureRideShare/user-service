69e885f3b6a021cf788fd759bc78c62b30f8d6a2
unknown <mcbride.mik@gmail.com>
2019-12-02 12:06:53 -0500

    Removing cross origin annotations.

--- src/main/java/com/revature/controller/UserControllerImpl.java
+++ src/main/java/com/revature/controller/UserControllerImpl.java
@@ -17,7 +17,6 @@ import javax.ws.rs.QueryParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +26,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
 public class UserControllerImpl implements UserController {
 
   UserService userService;
